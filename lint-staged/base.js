d9005ca1629e3f4b602bf2935bb7fe8cfff6c9a4
Alex Ilyaev <alexilyaev@gmail.com>
2025-02-17 09:58:13 +0200

    Fix regression in extending tsconfig configs

--- lint-staged/base.js
+++ lint-staged/base.js
@@ -4,6 +4,9 @@
  * export { default } from '@alexilyaev/configs/lint-staged/base.js';
  * ```
  */
+/**
+ * @type {Record<string, string | (filenames: string[]) => string | string[] | Promise<string | string[]>}
+ */
 const config = {
   '*.{js,cjs,ts,mts,tsx}': [
     'eslint --fix --max-warnings 0',
