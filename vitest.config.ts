18d945864b8074440c7d47fb4300359bca90968c
Alex Ilyaev <alexilyaev@gmail.com>
2025-04-01 12:41:21 +0300

    Bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,
     environment: 'node',
     exclude: [...configDefaults.exclude],
   },
