b9573c0c92d963781deca91b3febdd43ebc8fe64
Alex Ilyaev <alex_ilyaev@intuit.com>
2026-06-03 15:42:20 +0300

    chore: enhance error handling in publish-browserslist script

---
+++ lint-staged/base-no-eslint.js
@@ -0,0 +1,16 @@
+/**
+ * @code `lint-staged.config.js`:
+ * ```
+ * export { default } from '@alexilyaev/configs/lint-staged/base';
+ * ```
+ */
+/**
+ * @type {Record<string, string | (filenames: string[]) => string | string[] | Promise<string | string[]>}
+ */
+const config = {
+  '*.{js,cjs,ts,mts,tsx}': ['prettier --write'],
+  '*.{html,css,json,sql}': ['prettier --write'],
+  '*.md': ['prettier --write', 'rumdl check --fix'],
+};
+
+export default config;
